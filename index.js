ae7345d2840283668956dd857731f4c166696c45
Xhanticode <xhantimpu@gmail.com>
2022-06-30 11:08:15 +0200

    Hosting

---
+++ index.js
@@ -0,0 +1,14 @@
+const products = JSON.parse (localStorage.getItem("products")): [
+    {
+        
+    }
+]
+const products = [
+  {
+    category: document.getElementById().value,
+    title: document.getElementById().value,
+    price: document.getElementById().value,
+    year: document.getElementById().value,
+    img: document.getElementById().value,
+  }
+];
